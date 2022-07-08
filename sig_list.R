6ad3f979e5c400e3f87e899afd9d475a5c391bff
Yuting Tseng <michating5511@gmail.com>
2022-07-08 17:16:38 +0100

    added signature gene list

---
+++ sig_list.R
@@ -0,0 +1,4 @@
+#list of signature
+#aliases: DLG2 also named PSD93, DLG2 also named PSD93
+sig_high <- c("BASP1","CD9","DLG2","FNBP1","FRMD3","IGSF10","IL11RA","IQCE","KCNQ3","TOX2")
+sig_low <- c("CD147","CCDC125","CCDC125","CCDC125","CCDC125","HEBP2","HSD17B12")
